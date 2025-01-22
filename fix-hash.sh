1616029a709e67e51795df3367b6c0c224a69aea
danielk <daniel.kastinen@irf.se>
2025-01-22 17:15:39 +0100

    hugo templates and layouts now working for both book format and revealjs presentations

--- fix-hash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Magic sed to get hashes to work on pages with different subpaths
-# TODO: get someone competent in JS to make this generalized
-sed -i 's/presentations\//danielk-presentations\/presentations\//g' presentations/**/*.html
