95a271b38ac54dcbe45fc1fb3cc55224106c2860
danielk <daniel.kastinen@irf.se>
2025-01-24 16:23:09 +0100

    added that image classes are transfered on zoom

--- static/course.js
+++ static/course.js
@@ -3,12 +3,20 @@ function open_zoom(image) {
     var zoom_img = document.getElementById("zoom_img");
     zoom.style.display = "flex";
     zoom_img.src = image.src; // Set the clicked image as the zoom image
+    image.classList.forEach(className => {
+        if (className !== "expandable") {
+            zoom_img.classList.add(className);
+        }
+    });
 }
 
 function close_zoom() {
     var zoom = document.getElementById("image_zoom");
     zoom.style.display = "none";
     zoom_img.src = "";
+    zoom_img.classList.forEach(className => {
+        zoom_img.classList.remove(className);
+    });
 }
 
 
